d035044c3359172405dac206130e40f4ca200aec
Eric Van Dyn Hoven <eric.vandynhoven@gmail.com>
2017-04-25 17:11:57 -0700

    added first component to container div

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 // Create a new component. This component should produce html
-const App = function() {
+const App = () => {
   return <div>Hi!</div>;
 }
 
 // Take this component's html on put it on the page/DOM
+ReactDOM.render(<App />, document.querySelector('.container'));
