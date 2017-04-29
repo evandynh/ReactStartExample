90bb7d550b63a7663e9bf65638bae27996fdbe12
Eric Van Dyn Hoven <eric.vandynhoven@gmail.com>
2017-04-29 11:15:27 -0700

    added video list with thumbnail and title

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
+import YTSearch from 'youtube-api-search';
+import VideoList from './components/video_list';
+
 
 const API_KEY = 'AIzaSyBIXbACNY1HpDK0bHoRQo_kZqR8mk8_krE';
 
+
 // Create a new component. This component should produce html
-const App = () => {
-  return (
-    <div>
-      <SearchBar />
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+      this.setState({videos})
+      // this.setState({videos:videos}) only works if key and property are the same name
+    });
+  }
+  render () {
+    return (
+      <div>
+        <SearchBar />
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 // Take this component's html on put it on the page/DOM
